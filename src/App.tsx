3f21dc7e140d6f371551829a58135936b36011d5
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-02-27 07:47:32 +0200

    issue with drawbar!

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import Navbar from "./components/Navbar";
 
 export function App() {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <div className="container mx-auto ">
       {/* Navbar  */}
       <Navbar />
+      <p>Home</p>
       {/* Breadcrumb  */}
       {/* Introduction  */}
       {/* Featured Posts  */}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+    <div className=" h-16 md:h-20 flex items-center justify-between border border-red-500">
       {/* LOGO */}
       <div className="flex items-center gap-4 text-2xl font-bold">
         <img src="/logo.jpg" className="h-8 w-8" alt="logo" />
@@ -20,7 +20,9 @@ export default function Navbar() {
           {open ? "X" : "Menu"}
         </div>
         {/* MOBILE LINK LIST */}
-        <div className="w-full h-screen flex flex-col items-center absolute top-16 bg-red-500">menu</div>
+        <div className={`w-full h-screen flex flex-col items-center absolute z-10 top-16 bg-red-500 ${open ? "-right-0" : "-right-[100%]"} ` }>
+          menu
+        </div>
       </div>
       {/*DESKTOP MENU */}
       <div className="hidden md:flex">D</div>
